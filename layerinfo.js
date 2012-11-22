e63cbe8873f1ed4b1c0dec4c567d85d13228afee
matt.walker <none@none>
2012-11-22 08:56:34 +0000

    Wait until jQuery is available and the layer control has draw, plus other minor changes

--- layerinfo.js
+++ layerinfo.js
@@ -1,16 +1,18 @@
-// Add an entry for each Layer: layer name as found in the mapfile and the text to be displayed
+// Add an entry for each layer. The layer name should be as found in the mapfile and the text to be displayed
 astun.layersHoverInfo = {
-    'wards' : 'Ward boundaries maintained by the Office of National Statistics',
-    'rightsofway' : 'Rights of way routes are approximately plotted, hence indicative only.'
+    "Wards" : "Ward boundaries maintained by the Office of National Statistics",
+    "SchoolsPrim" : "Primary Schools maintained by the Local Authority"
 };
 
 (function() {
 
+    // Wait until all of our dependencies are
+    // met such as jQuery being loaded, we're on
+    // a map page and the layer control has loaded
     var timer = setInterval(function() {
         if (typeof jQuery != 'undefined') {
             if(jQuery('#atMyMaps').length) {
-                var panels = jQuery('#panels');
-                if (panels.length) {
+                if (jQuery('div#showmapcategories .atLayerSet').length) {
                     clearInterval(timer);
                     layerinfo();
                 }
@@ -20,18 +22,23 @@ astun.layersHoverInfo = {
         }
     }, 100);
 
+    // Adds a tooltip to each of the layers in the
+    // astun.layersHoverInfo list
     function layerinfo() {
 
+        // Append the tooltip to the page and hide it
         jQuery('body').append(
             jQuery('<div/>')
                 .hide()
                 .addClass('atLayerInfoTooltip ui-state-default ui-corner-all')
-                .append('<div><span class="ui-icon ui-icon-triangle-1-n"></span><span class="text"></span></div>')
+                .append('<div><span class="ui-icon ui-icon-triangle-1-n"></span><div class="text"></div></div>')
         );
 
+        // Add listeners to each layer in the list and decorate the
+        // layer name with an info icon
         jQuery.each(astun.layersHoverInfo, function(key, value) {
             jQuery('li[aria-describedby="'+key+'"]')
-                .find('a span')
+                .find('a > span')
                 .live('mouseover', function(){
                     var t = jQuery(this).offset().top + jQuery(this).parents('li').height() + 10;
                     var l = jQuery(this).parents('li').offset().left;
@@ -39,7 +46,7 @@ astun.layersHoverInfo = {
                     jQuery('.atLayerInfoTooltip')
                         .css({'top':t, 'left':l, 'width':w})
                         .show()
-                        .find('div span:last').html(value);
+                        .find('.text').html(value);
                 })
                 .live('mouseout', function(){
                     jQuery('.atLayerInfoTooltip').hide();
